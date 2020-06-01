ac80c45b0e179ec1ecf929c6a6d5ba3a5b061e28
John Fulton <fulton@redhat.com>
2020-06-01 19:42:18 +0000

    poc/no-metalsmith.sh should ignore compute nodes without OSD disks

--- poc/no-metalsmith.sh
+++ poc/no-metalsmith.sh
@@ -9,6 +9,9 @@ MAP[oc0-controller-2]="0-controller-2"  # Controller
 MAP[oc0-ceph-0]="0-compute-0"           # Compute
 MAP[oc0-ceph-1]="0-compute-1"           # Compute
 MAP[oc0-ceph-2]="0-compute-2"           # Compute
+MAP[oc0-compute-0]="0-ignore-0"         # ignore
+MAP[oc0-compute-1]="0-ignore-1"         # ignore
+
 
 for K in "${!MAP[@]}"; do
     echo "$K ---> ${MAP[$K]}";
