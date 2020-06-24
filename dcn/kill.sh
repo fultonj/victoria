21540fe810743e0085b2b0ebf14c4f29d53acf06
John Fulton <fulton@redhat.com>
2020-06-24 01:44:11 +0000

    dcn/kill.sh rm temp files

--- dcn/kill.sh
+++ dcn/kill.sh
@@ -3,3 +3,8 @@
 for STACK in $(openstack stack list -f value -c "Stack Name"); do
     openstack overcloud delete $STACK --yes
 done
+
+rm -f control-plane/ceph_keys.yaml
+rm -f control-plane/ceph_keys_update.yaml
+rm -f ~/control-plane-export.yaml
+rm -f ~/dcn_ceph_keys.yaml
