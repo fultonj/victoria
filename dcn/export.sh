0901ecb2beafc2069627a311010f9cae9cf9d1a5
John Fulton <fulton@redhat.com>
2020-06-30 20:24:31 +0000

    Update dcn/export.sh to use ~/config-download
    This change depends on the following changes:
    
     https://review.opendev.org/737020
     https://review.opendev.org/737671
    
    which merged to address the following bug:
    
     https://bugs.launchpad.net/tripleo/+bug/1884246
    
    This change assumes that the person who deployed
    DCN did not use --stack-only and that they let the
    config-download process get run directly from the
    'openstack overcloud deploy' command.

--- dcn/export.sh
+++ dcn/export.sh
@@ -13,19 +13,13 @@ if [[ -e ~/${STACK}-export.yaml ]]; then
     rm -f ~/${STACK}-export.yaml
 fi
 
-if [[ ! -d $STACK/config-download/$STACK ]]; then
-    echo "workaround https://bugs.launchpad.net/tripleo/+bug/1884246"
-    mkdir $STACK/config-download/$STACK
-    pushd $STACK/config-download/$STACK/
-    ln -s ../group_vars group_vars
-    popd
-fi
-
 openstack overcloud export \
-          --config-download-dir $STACK/config-download/ \
           --stack $STACK \
           --output-file ~/${STACK}-export.yaml
+
 if [[ ! -e ~/${STACK}-export.yaml ]]; then
     echo "Unable to create ~/${STACK}-export.yaml. Aborting."
     exit 1
 fi
+
+# --config-download-dir $STACK/config-download/ \
