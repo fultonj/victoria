9644c65da947ae42abffdc2e0911a68aece59c5a
John Fulton <fulton@redhat.com>
2020-07-27 20:36:47 +0000

    update kill to clean nodes

--- standard/kill.sh
+++ standard/kill.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+CLEAN=1
+if [[ $CLEAN -eq 1 ]]; then
+    cat /dev/null > /tmp/to_clean
+    for S in $(openstack server list -f value -c Name | grep ceph); do
+        echo $S >> /tmp/to_clean
+    done
+fi
+
 openstack overcloud delete oc0 --yes
 #openstack overcloud node unprovision --yes --all --stack oc0 metal-big.yaml
+
+if [[ $CLEAN -eq 1 ]]; then
+    for S in $(cat /tmp/to_clean); do
+        bash ../metalsmith/clean-disks.sh $S
+    done
+    rm /tmp/to_clean
+fi
