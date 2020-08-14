b9e33e4810c884fdddc7f77fd51414357224c286
John Fulton <fulton@redhat.com>
2020-08-14 14:46:24 +0000

    add clean option to dcn/kill

--- dcn/kill.sh
+++ dcn/kill.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+CLEAN=1
+source ~/stackrc
+
+if [[ $CLEAN -eq 1 ]]; then
+    cat /dev/null > /tmp/ironic_names_to_clean
+    openstack server list -f value -c Name -c ID | grep hci | awk {'print $1'} > /tmp/nova_ids_to_clean
+    cat /dev/null > /tmp/ironic_names_to_clean
+    for S in $(cat /tmp/nova_ids_to_clean); do
+        openstack baremetal node list -f value -c Name -c "Instance UUID" | grep $S | awk {'print $1'} >> /tmp/ironic_names_to_clean
+    done
+fi
+
 for STACK in $(openstack stack list -f value -c "Stack Name"); do
     openstack overcloud delete $STACK --yes
 done
@@ -8,3 +20,9 @@ rm -f control-plane/ceph_keys.yaml
 rm -f control-plane/ceph_keys_update.yaml
 rm -f ~/control-plane-export.yaml
 rm -f ~/dcn_ceph_keys.yaml
+
+if [[ $CLEAN -eq 1 ]]; then
+    for S in $(cat /tmp/ironic_names_to_clean); do
+        bash ../metalsmith/clean-disks.sh $S
+    done
+fi
